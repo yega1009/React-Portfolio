dfe92b1683c06609031b546279c48629508286b8
Yega Cho <yega1009@gmail.com>
2023-12-01 02:44:45 -0500

    created PortfolioContainer functional component with a function to render page based on the current page

--- src/components/PortfolioContainer.jsx
+++ src/components/PortfolioContainer.jsx
@@ -1,4 +1,4 @@
-// Importing necessary React hooks and components
+// Import necessary React hooks and components
 import { useState } from 'react';
 import Header from './Header';
 import AboutMe from './pages/AboutMe';
@@ -7,3 +7,22 @@ import Portfolio from './pages/Portfolio';
 import Resume from './pages/Resume';
 import Footer from './Footer';
 
+// Define the PortfolioContainer functional component
+export default function PortfolioContainer() {
+  // Use useState to manage the current page state
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  // Function to render the page based on the current page state
+  const renderPage = () => {
+    if (currentPage === 'Contact') {
+      return <Contact />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'Resume') {
+      return <Resume />;
+    }
+    return <AboutMe />;
+  };
+}
