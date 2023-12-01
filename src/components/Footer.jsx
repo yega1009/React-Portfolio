f89d58c1be0e6330e5bf2d21673ec9fb74a6863e
Yega Cho <yega1009@gmail.com>
2023-12-01 02:55:15 -0500

    created function to display footer section with social media links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,20 @@
 import React from 'react';
 import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa'; // Importing icons from react-icons library
 import './styles/Footer.css';
+
+// Displays the footer section with social media icons
+export default function Footer() {
+  return (
+    // Footer container
+    <footer className="footer">
+      {/* GitHub link with icon */}
+      <a href="https://github.com/yega1009" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
+
+      {/* LinkedIn link with icon */}
+      <a href="https://www.linkedin.com/in/yega-cho-4238681a6/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
+
+      {/* Twitter link and icon */}
+      <a href="https://twitter.com/yega1009" target="_blank" rel="noopener noreferrer"><FaTwitter /></a>
+    </footer>
+  );
+}
