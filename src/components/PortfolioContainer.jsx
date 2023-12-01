5ae890d37f32f5f33abbfcc1eb53a976cb585d97
Yega Cho <yega1009@gmail.com>
2023-12-01 02:42:05 -0500

    imported necessary react components

---
+++ src/components/PortfolioContainer.jsx
@@ -0,0 +1,9 @@
+// Importing necessary React hooks and components
+import { useState } from 'react';
+import Header from './Header';
+import AboutMe from './pages/AboutMe';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+import Footer from './Footer';
+
